5bc16390d69a511a7e9ebe18a90e67608a674cfc
himanshusingh161500239 <37707707+himanshusingh161500239@users.noreply.github.com>
2018-10-15 19:09:08 +0530

    Add files via upload

---
+++ Alphabet.py
@@ -0,0 +1,9 @@
+def f1():
+    ch=input()
+    if(ord(ch)>=65 and ord(ch)<=90):
+        print("Alphabet")
+    elif(ord(ch)>=97 and ord(ch)<=122):
+        print("Alphabet")
+    else:
+        print("No")
+f1()
